7b93832e029cb9cab06ac0794ec5b18581b12ec1
LazyShpee <comemureravaud@gmail.com>
2017-12-11 12:27:01 +0900

    fixed indent in colormask

--- libs/color_mask.js
+++ libs/color_mask.js
@@ -1,44 +1,44 @@
 const Jimp = require('jimp')
 
 /*
-	{
-		base: 'path',
-		masks: [
-			{
-				image: 'path',
-				color: [r, g, b]
-			}
-		]
-	}
+  {
+    base: 'path',
+    masks: [
+      {
+        image: 'path',
+        color: [r, g, b]
+      }
+    ]
+  }
 */
 
 function _colorize(image, options, index, callback) {
-	if (index < options.masks.length) {
-		Jimp.read(options.masks[index].image).then(function(mask) {
-			if (image.bitmap.width !== mask.bitmap.width || image.bitmap.height !== mask.bitmap.height) {
-				return callback('Error coloring mask ' + index + ', mask and base image size should match');
-			}
-			for (var i = 0; i < image.bitmap.width * image.bitmap.height * 4; i += 4) {
-				if (mask.bitmap.data[i] === 255 && mask.bitmap.data[i + 1] === 255 && mask.bitmap.data[i + 2] === 255) {
-					for (var n = 0; n < 3; n++)
-						image.bitmap.data[i + n] = options.masks[index].color[n];
-				}
-			}
-			_colorize(image, options, index + 1, callback);
-		}).catch(function(err) {
-			callback(err);
-		});
-	} else {
-		callback(null, image);
-	}
+  if (index < options.masks.length) {
+    Jimp.read(options.masks[index].image).then(function(mask) {
+      if (image.bitmap.width !== mask.bitmap.width || image.bitmap.height !== mask.bitmap.height) {
+        return callback('Error coloring mask ' + index + ', mask and base image size should match');
+      }
+      for (var i = 0; i < image.bitmap.width * image.bitmap.height * 4; i += 4) {
+        if (mask.bitmap.data[i] === 255 && mask.bitmap.data[i + 1] === 255 && mask.bitmap.data[i + 2] === 255) {
+          for (var n = 0; n < 3; n++)
+            image.bitmap.data[i + n] = options.masks[index].color[n];
+        }
+      }
+      _colorize(image, options, index + 1, callback);
+    }).catch(function(err) {
+      callback(err);
+    });
+  } else {
+    callback(null, image);
+  }
 }
 
 exports.generate = function(callback, options) {
-	Jimp.read(options.base)
-	.then(function(image) {
-		_colorize(image, options, 0, callback);
-	})
-	.catch(function(err) {
-		callback(err);
-	});
+  Jimp.read(options.base)
+  .then(function(image) {
+    _colorize(image, options, 0, callback);
+  })
+  .catch(function(err) {
+    callback(err);
+  });
 }
