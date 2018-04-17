539d2b8aea5fe15339fcc15ab1cadf70273414a5
LazyShpee <comemureravaud@gmail.com>
2018-04-17 22:32:32 +0900

    Autocrop and auto center (experimental)

--- generators/search.js
+++ generators/search.js
@@ -4,13 +4,15 @@ const helper = require('../helper');
 // 158 x 64
 
 async function _search(_text) {
+    const tw = 172, th = 74, tx = 54, ty = 328;
+
     let font = await Jimp.loadFont('./ressources/fonts/DILBERT.fnt');
     let base = await Jimp.read('./ressources/images/the-search.png');
-    let text = await new Jimp(158, 74);
+    let text = await new Jimp(tw, th);
 
-    text.print(font, 3, 0, _text, 154).crop(0,0,158,73);
+    text.print(font, 3, 0, _text, tw - 8).crop(0 ,0 , tw - 2, th - 2).autocrop();
 
-    base.composite(text, 60, 330);
+    base.composite(text, tx + (tw - text.bitmap.width) / 2, ty + (th - text.bitmap.height) / 2);
 
     return base;
 }
